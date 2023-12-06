2e905df712d68c3f8162f607e0e0afe57418f4ac
Smna <ghimiresamana666@gmail.com>
2023-12-06 15:52:39 +0545

    test: any

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,6 +42,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 2,
-    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
